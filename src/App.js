77db94129e74fcb225737da396807069e2ef649d
rashidxon <nuriddinovrashidxon2002@gmail.com>
2023-11-21 10:39:37 +0500

    Game site

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Cards from './components/Cards';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rashidxon</h1>
+      <Cards/>
+    </div>
+  );
+}
+
+export default App;
